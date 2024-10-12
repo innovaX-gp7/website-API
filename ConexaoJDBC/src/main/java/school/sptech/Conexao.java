caba7ab0fd2b3c58e9b62cf48669ec5baa14a488
unknown <luiz.mendonca@sptech.school>
2024-10-12 13:14:58 -0300

    adicionando senha para testes

--- ConexaoJDBC/src/main/java/school/sptech/Conexao.java
+++ ConexaoJDBC/src/main/java/school/sptech/Conexao.java
@@ -13,7 +13,7 @@ public class Conexao {
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/InnovaxDB"); // Aqui fica a URL do nosso banco de dados
         dataSource.setUsername("root"); // Aqui fica o usuário do Banco de Dados
-        dataSource.setPassword("@"); // Aqui fica a senha do usuário
+        dataSource.setPassword("Urubu100"); // Aqui fica a senha do usuário
 
         // Inicializando o JdbcTemplate com o DataSource configurado
         this.conexaoDoBanco = new JdbcTemplate(dataSource);
