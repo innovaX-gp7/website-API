03bc5c28976a30fef7e5e39e4730468c5d72e7d1
unknown <luiz.mendonca@sptech.school>
2024-10-12 14:56:07 -0300

    Adiciona comentarios e codigo de baixar arquivos do s3

--- ConexaoJDBC/src/main/java/school/sptech/s3/BucketController.java
+++ ConexaoJDBC/src/main/java/school/sptech/s3/BucketController.java
@@ -1,16 +1,55 @@
 package school.sptech.s3;
 
+import software.amazon.awssdk.core.sync.ResponseTransformer;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.Bucket;
+import software.amazon.awssdk.services.s3.model.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.util.List;
 
 public class BucketController {
     public static void listarBuckets() {
+//        Instancia uma conexão com o S3
         S3Client s3Client = new S3Provider().getS3Client();
+
+//        Lista buckets
         List<Bucket> buckets = s3Client.listBuckets().buckets();
-        for (Bucket bucket : buckets) {
-            System.out.println("Bucket: " + bucket.name());
+
+//        Cria bucket da Innovax caso não exista
+        if(buckets == null || buckets.isEmpty()){
+            CreateBucketRequest createBucketRequest = CreateBucketRequest.builder()
+                    .bucket("innovaxs3")
+                    .build();
+            s3Client.createBucket(createBucketRequest);
+        }
+
+//        Cria requisição para listar os objetos dentro do bucket
+        ListObjectsRequest listObjectsRequest = ListObjectsRequest.builder()
+                .bucket("innovaxs3")
+                .build();
+
+//        Lista objetos do bucket usando a requisição
+        List<S3Object> objects = s3Client.listObjects(listObjectsRequest).contents();
+
+        for (S3Object object : objects) {
+//            Cria requisição para baixar um objeto do bucket
+            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                    .bucket("nome-do-bucket")
+                    .key(object.key())
+                    .build();
+
+//            Captura objeto usando a requisição e tenta transformar para formato de arquivo que o java interpreta
+            InputStream objectContent = s3Client.getObject(getObjectRequest, ResponseTransformer.toInputStream());
+
+            try {
+//                Copia arquivo do s3 para um arquivo local com o nome da key do objeto
+                Files.copy(objectContent, new File(object.key()).toPath());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
--- ConexaoJDBC/src/main/java/school/sptech/s3/S3Provider.java
+++ ConexaoJDBC/src/main/java/school/sptech/s3/S3Provider.java
@@ -6,6 +6,7 @@ import software.amazon.awssdk.services.s3.S3Client;
 
 public class S3Provider {
     public S3Client getS3Client() {
+//      Cria Conexão com o s3 usando configurações locais da EC2
         return S3Client.builder()
                 .region(Region.US_EAST_1)
                 .credentialsProvider(DefaultCredentialsProvider.create())
